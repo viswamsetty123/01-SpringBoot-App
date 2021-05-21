0b6e304b92cbdd9b7936a76cd9418da305d89a56
Ashok Kumar Viswamsetty <viswamsettyashokkumar@gmail.com>
2021-05-21 18:36:01 +0530

    feature123 changes completed

--- src/main/java/com/ashok/Application.java
+++ src/main/java/com/ashok/Application.java
@@ -16,7 +16,7 @@ public class Application {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
 		System.out.println(run.getBean("UserDAO.Class"));
-		
+		int i=10;
 		System.out.println("first spring boot app");
 		System.out.println("didnt close context object");
 		//run.close();
